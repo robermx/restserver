fa0cdc3dc9d7e19c782f4eefba7a425cec42f1eb
Roberto Arenas <roberto.arenas2@icloud.com>
2021-04-12 18:29:02 +0000

    Quitando LogOut del index

--- public/js/auth.js
+++ public/js/auth.js
@@ -52,10 +52,3 @@ function onSignIn(googleUser) {
     })
     .catch(console.log());
 }
-
-function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    console.log('User signed out.');
-  });
-}
